85def90fdee0d2cf90bd5b3145656b264e62b5fd
Gilberto-Vera <rasvejr@gmail.com>
2019-11-01 19:59:22 -0200

    Ajuste do projeto

--- src/model/Conexao2.java
+++ src/model/Conexao2.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Conexao {
+public class Conexao2 {
     public static Connection getConexao(){
         String driver = "org.postgresql.Driver";
         String URL = "jdbc:postgresql://localhost:5432/aps_bd2";
@@ -21,7 +21,7 @@ public class Conexao {
             System.err.print(e.getMessage());
             return null;
         } catch (ClassNotFoundException ex) {
-            Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Conexao2.class.getName()).log(Level.SEVERE, null, ex);
             return null;
         }
     }
